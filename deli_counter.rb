e993bbd9749f817ccd36af7e34c56f8f188ea516
Nory Lazo <nlazo@icloud.com>
2019-11-10 23:10:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -28,7 +28,7 @@ def now_serving(katz_deli)
   if katz_deli.empty? == true 
     puts "There is nobody waiting to be served!"
   elsif katz_deli.empty? == false 
-    puts "Currently serving #{katz_deli.shift}"
+    puts "Currently serving #{katz_deli[]}"
   end
     
 end
