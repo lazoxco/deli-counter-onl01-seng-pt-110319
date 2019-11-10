924ae485980fa30b7eee38cad818fe206912d6e6
Nory Lazo <nlazo@icloud.com>
2019-11-10 23:09:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -29,8 +29,7 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   elsif
     array.each do |name|
-      puts "Currently serving #{name}."
-      
+
     end 
   end
     
