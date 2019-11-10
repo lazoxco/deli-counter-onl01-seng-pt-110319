26b7fca49d8f23279b81b607a1df8388bc2d0d4f
Nory Lazo <nlazo@icloud.com>
2019-11-10 23:06:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 # Write your code here.
 
-def line(katz_deli)
-  if katz_deli.length == 0 
+def line(array)
+  if array.length == 0 
     puts "The line is currently empty."
   else
     message = "The line is currently:"
-    katz_deli.each_with_index do |value, index|
+    array.each_with_index do |value, index|
       message += " #{index + 1}. #{value}"
     end 
     puts message
@@ -24,6 +24,7 @@ def take_a_number(katz_deli, name)
   end
 end
 
-def now_serving()
-  if 
+def now_serving(katz_deli)
+  if katz_deli.empty? == true 
+    
 end
